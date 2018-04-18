d532c57ebc2dd24372672f638f0563fd3d7f9053
Michele Iessi <iessimichele@gmail.com>
2018-04-18 12:24:39 +0200

    Commit per Silvia

---
+++ Client/src/View/core/src/com/debellonavali/ProvaDragAndDrop.java
@@ -0,0 +1,102 @@
+package com.debellonavali;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.utils.DragAndDrop;
+
+public class ProvaDragAndDrop extends Game {
+    Stage stage;
+
+    public void create () {
+        stage = new Stage();
+        Gdx.input.setInputProcessor(stage);
+
+        final Skin skin = new Skin();
+        skin.add("default", new Label.LabelStyle(new BitmapFont(), Color.WHITE));
+        skin.add("badlogic", new Texture(Constants.BATTLE_BACKGROUND_IMG));
+
+        Image sourceImage = new Image(skin, "badlogic");
+        sourceImage.setBounds(50, 125, 100, 100);
+        stage.addActor(sourceImage);
+
+        Image validTargetImage = new Image(skin, "badlogic");
+        validTargetImage.setBounds(200, 50, 100, 100);
+        stage.addActor(validTargetImage);
+
+        Image invalidTargetImage = new Image(skin, "badlogic");
+        invalidTargetImage.setBounds(200, 200, 100, 100);
+        stage.addActor(invalidTargetImage);
+
+        DragAndDrop dragAndDrop = new DragAndDrop();
+        dragAndDrop.addSource(new DragAndDrop.Source(sourceImage) {
+            public DragAndDrop.Payload dragStart (InputEvent event, float x, float y, int pointer) {
+                DragAndDrop.Payload payload = new DragAndDrop.Payload();
+                payload.setObject("Some payload!");
+
+                payload.setDragActor(new Label("Some payload!", skin));
+
+                Label validLabel = new Label("Some payload!", skin);
+                validLabel.setColor(0, 1, 0, 1);
+                payload.setValidDragActor(validLabel);
+
+                Label invalidLabel = new Label("Some payload!", skin);
+                invalidLabel.setColor(1, 0, 0, 1);
+                payload.setInvalidDragActor(invalidLabel);
+
+                return payload;
+            }
+        });
+        dragAndDrop.addTarget(new DragAndDrop.Target(validTargetImage) {
+            public boolean drag (DragAndDrop.Source source, DragAndDrop.Payload payload, float x, float y, int pointer) {
+                getActor().setColor(Color.GREEN);
+                return true;
+            }
+
+            public void reset (DragAndDrop.Source source, DragAndDrop.Payload payload) {
+                getActor().setColor(Color.WHITE);
+            }
+
+            public void drop (DragAndDrop.Source source, DragAndDrop.Payload payload, float x, float y, int pointer) {
+                System.out.println("Accepted: " + payload.getObject() + " " + x + ", " + y);
+            }
+        });
+        dragAndDrop.addTarget(new DragAndDrop.Target(invalidTargetImage) {
+            public boolean drag (DragAndDrop.Source source, DragAndDrop.Payload payload, float x, float y, int pointer) {
+                getActor().setColor(Color.RED);
+                return false;
+            }
+
+            public void reset (DragAndDrop.Source source, DragAndDrop.Payload payload) {
+                getActor().setColor(Color.WHITE);
+            }
+
+            public void drop (DragAndDrop.Source source, DragAndDrop.Payload payload, float x, float y, int pointer) {
+                System.out.println("Refused: " + payload.getObject() + " " + x + ", " + y);
+            }
+        });
+    }
+
+    public void render () {
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        stage.act(Gdx.graphics.getDeltaTime());
+        stage.draw();
+    }
+
+    public void resize (int width, int height) {
+        stage.getViewport().update(width, height, true);
+    }
+
+    public void dispose () {
+        stage.dispose();
+    }
+
+}
