3e8b6533d94b1638b3ebc60ffd5eeb70687297bf
Michele Iessi <iessimichele@gmail.com>
2018-05-31 11:17:09 +0200

    FacadeClientController JavaDoc

--- Client/src/View/core/src/com/debellonavali/Classes/Controller/FacadeClientController.java
+++ Client/src/View/core/src/com/debellonavali/Classes/Controller/FacadeClientController.java
@@ -6,36 +6,61 @@ import com.debellonavali.Classes.Controller.Observers.IObserverController;
 import java.util.ArrayList;
 import java.util.logging.Logger;
 
+/**
+ * FacadeClientController is the unique access point of a socket communication to the system
+ */
 public class FacadeClientController implements IClientController {
 
     private String function;
     private IDTO dto;
     private ArrayList<IObserverController> clientObservers;
 
+    /**
+     * Default constructor. It instantiates an empty ArrayList of IObserverController objects.
+     */
     public FacadeClientController() {
         this.clientObservers = new ArrayList<>();
     }
 
+    /**
+     * Returns the function of the last received communication
+     * @return The function of the last received communication
+     */
     @Override
     public String getFunction() {
         return this.function;
     }
 
+    /**
+     * Returns the DTO contained in the last received communication
+     * @return The DTO contained in the last received communication
+     */
     @Override
     public IDTO getDTO() {
         return this.dto;
     }
 
+    /**
+     * Attaches an IObserverController object to this Facade
+     * @param observer The IObserverController to be attached
+     */
     @Override
     public void attachObserver(IObserverController observer) {
         this.clientObservers.add(observer);
     }
 
+    /**
+     * Detaches an IObserverController object form this Facade
+     * @param observer The IObserverController to be detached
+     */
     @Override
     public void detachObserver(IObserverController observer) {
         this.clientObservers.remove(observer);
     }
 
+    /**
+     * Notifies all IObserverController attached to this Facade
+     */
     @Override
     public void notifyObservers() {
         for (IObserverController observer : clientObservers) {
@@ -43,20 +68,15 @@ public class FacadeClientController implements IClientController {
         }
     }
 
+    /**
+     * Processes an incoming DTO object and notifies the IObserverController attached to this Facade
+     * @param incomingDTO
+     */
     @Override
     public void incomingRequest(IDTO incomingDTO) {
         this.function = incomingDTO.getFunctionString();
         this.dto = incomingDTO;
         Logger.getAnonymousLogger().info("DTO received");
         notifyObservers();
-    }
-
-    @Override
-    public String toString() {
-        return "FacadeClientController{" +
-                "function='" + function + '\'' +
-                ", dto=" + dto +
-                ", clientObservers=" + clientObservers +
-                '}';
     }
 }
