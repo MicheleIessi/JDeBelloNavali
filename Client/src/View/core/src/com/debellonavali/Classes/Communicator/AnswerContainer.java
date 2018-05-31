acc72efda8d34d96ab308e54826c370ff9d7f4e1
Michele Iessi <iessimichele@gmail.com>
2018-05-31 11:04:05 +0200

    AnswerContainer JavaDoc

--- Client/src/View/core/src/com/debellonavali/Classes/Communicator/AnswerContainer.java
+++ Client/src/View/core/src/com/debellonavali/Classes/Communicator/AnswerContainer.java
@@ -2,24 +2,36 @@ package com.debellonavali.Classes.Communicator;
 
 import com.debellonavali.Classes.Communicator.DTO.IDTO;
 
+/**
+ * AnswerContainer is used to retrieve the response of an Observer Controller
+ */
 public class AnswerContainer {
 
     private static AnswerContainer instance = null;
     private IDTO storedDTO = null;
 
-    private AnswerContainer() {}
-
+    /**
+     * Puts a IDTO object into the container
+     * @param dtoMessage
+     */
     public void putDTOMessage(IDTO dtoMessage) {
         this.storedDTO = dtoMessage;
     }
 
+    /**
+     * Gets the stored IDTO object
+     * @return
+     */
     public IDTO getStoredDTO() {
         IDTO dtoAnswer = this.storedDTO;
         this.storedDTO = null;
         return dtoAnswer;
-
     }
 
+    /**
+     * Singleton class: only one instance of this class is allowed to be instantiated
+     * @return The single instance of AnswerContainer
+     */
     public static AnswerContainer getInstance() {
         if(instance == null) {
             instance = new AnswerContainer();
