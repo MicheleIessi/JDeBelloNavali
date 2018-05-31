ce5b61c76b74665ee147b927a7790f87db0aaac1
Michele Iessi <iessimichele@gmail.com>
2018-05-31 10:56:53 +0200

    DTOBuilder JavaDoc

--- Client/src/View/core/src/com/debellonavali/Classes/Communicator/DTO/DTOBuilder/DTOBuilder.java
+++ Client/src/View/core/src/com/debellonavali/Classes/Communicator/DTO/DTOBuilder/DTOBuilder.java
@@ -8,10 +8,18 @@ import java.util.List;
 import java.util.Map;
 import java.util.logging.Logger;
 
+/**
+ * Class responsible to create different DTO Objects based on their purpose
+ */
 public class DTOBuilder implements IDTOBuilder {
 
     private static DTOBuilder instance = null;
 
+    /**
+     * Creates a DTO that contains an attack. It is used by an attacker that wants to attack
+     * @param attackedSquares List of attacked coordinates
+     * @return An Attack DTO
+     */
     @Override
     public IDTO createAttackDTO(List<int[]> attackedSquares) {
         Logger.getLogger(getClass().getSimpleName()).info("Created attack DTO");
@@ -20,6 +28,11 @@ public class DTOBuilder implements IDTOBuilder {
         return attackDTO;
     }
 
+    /**
+     * Creates a DTO that contains an attack's result. It is used by a defender after receiving an attack
+     * @param attackResults Map of attack results
+     * @return An AttackResult DTO
+     */
     @Override
     public IDTO createAttackResultsDTO(Map<int[], Boolean> attackResults) {
         Logger.getLogger(getClass().getSimpleName()).info("Created attack result DTO for who has attacked");
@@ -28,6 +41,12 @@ public class DTOBuilder implements IDTOBuilder {
         return attackResultsDTO;
     }
 
+    /**
+     * Creates a DTO that contains a defence result. It is used by a defender to update its View in order to
+     * visualize a received attack
+     * @param defenceResult Map of attack results
+     * @return A DefenceResult DTO
+     */
     @Override
     public IDTO createDefenceResultsDTO(Map<int[], Boolean> defenceResult) {
         Logger.getLogger(getClass().getSimpleName()).info("Created attack result DTO for who has defended");
@@ -36,6 +55,11 @@ public class DTOBuilder implements IDTOBuilder {
         return attackResultsDTO;
     }
 
+    /**
+     * Creates a DTO that will be recognized as a game win
+     * @param attackerWin Map that contains a game win information
+     * @return An AttackWin DTO
+     */
     @Override
     public IDTO createAttackWinDTO(Map<int[], Boolean> attackerWin) {
         Logger.getLogger(getClass().getSimpleName()).info("Created WIN DTO for attacker ");
@@ -44,6 +68,11 @@ public class DTOBuilder implements IDTOBuilder {
         return attackResultsDTO;
     }
 
+    /**
+     * Creates a DTO that will be recognized as a game loss
+     * @param defenderLose Map that contains a game loss information
+     * @return A DefenderLose DTO
+     */
     @Override
     public IDTO createDefenceLoseDTO(Map<int[], Boolean> defenderLose) {
         Logger.getLogger(getClass().getSimpleName()).info("Created LOSE DTO for defender ");
@@ -53,6 +82,10 @@ public class DTOBuilder implements IDTOBuilder {
 
     }
 
+    /**
+     * Returns the only instance allowed to be instantiated
+     * @return The DTOBuilder instance
+     */
     public static  DTOBuilder getInstance() {
         if (instance == null) {
             instance = new DTOBuilder();
